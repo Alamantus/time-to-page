d2a96b7e1d01a9597b639620f981a010978c9a55
Robbie Antenesse <dev@alamantus.com>
2020-01-08 15:27:05 -0700

    Make TimeColumns smarter and auto-increment upper values

--- src/components/time/TimeColumns.js
+++ src/components/time/TimeColumns.js
@@ -3,6 +3,44 @@ import PropTypes from 'prop-types';
 
 const TimeColumns = (props) => {
   const idPrefix = props.idPrefix;
+
+  const updateHours = value => {
+    if (isNaN(value)) value = 0;
+    props.updateHours(value);
+  }
+
+  const updateMinutes = value => {
+    if (isNaN(value)) value = 0;
+    if (value < 0) {
+      if (props.hours > 0) {
+        updateHours(props.hours - 1);
+        value = 59;
+      } else {
+        value = 0;
+      }
+    } else if (value > 59) {
+      updateHours(props.hours + 1);
+      value = 0;
+    }
+    props.updateMinutes(value);
+  }
+
+  const updateSeconds = value => {
+    if (isNaN(value)) value = 0;
+    if (value < 0) {
+      if (props.minutes > 0) {
+        updateMinutes(props.minutes - 1);
+        value = 59;
+      } else {
+        value = 0;
+      }
+    } else if (value > 59) {
+      updateMinutes(props.minutes + 1);
+      value = 0;
+    }
+    props.updateSeconds(value);
+  }
+
   return <article className="row">
     <div className="col form-group">
       <label htmlFor={idPrefix + 'Hours'}>Hours</label>
@@ -10,35 +48,26 @@ const TimeColumns = (props) => {
         type="number" step="1" min="0"
         value={props.hours}
         onFocus={event => event.target.select()}
-        onChange={event => {
-          let { value } = event.target;
-          if (isNaN(parseInt(value))) value = 0;
-          props.updateHours(value);
-        }} />
+        onChange={event => updateHours(parseInt(event.target.value))}
+      />
     </div>
     <div className="col form-group">
       <label htmlFor={idPrefix + 'Minutes'}>Minutes</label>
       <input className="form-control" id={idPrefix + 'Minutes'}
-        type="number" step="1" min="0" max="60"
+        type="number" step="1" min="-1" max="60"
         value={props.minutes}
         onFocus={event => event.target.select()}
-        onChange={event => {
-          let { value } = event.target;
-          if (isNaN(parseInt(value))) value = 0;
-          props.updateMinutes(value);
-        }} />
+        onChange={event => updateMinutes(parseInt(event.target.value))}
+      />
     </div>
     <div className="col form-group">
       <label htmlFor={idPrefix + 'Seconds'}>Seconds</label>
       <input className="form-control" id={idPrefix + 'Seconds'}
-        type="number" step="1" min="0" max="60"
+        type="number" step="1" min="-1" max="60"
         value={props.seconds}
         onFocus={event => event.target.select()}
-        onChange={event => {
-          let { value } = event.target;
-          if (isNaN(parseInt(value))) value = 0;
-          props.updateSeconds(value);
-        }} />
+        onChange={event => updateSeconds(parseInt(event.target.value))}
+      />
     </div>
   </article>;
 }
